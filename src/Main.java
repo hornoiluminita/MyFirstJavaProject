6b537d1ac7422b33d1d7b10405735429f38c14c9
cosminciocan <ciocan.cosmin@gmail.com>
2019-02-06 19:57:22 +0200

    modificat ex1

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(1+2);
         System.out.println(1+2);
-        System.out.println(1+2);
-        System.out.println(1+2);
-        System.out.println(1+2);
-        System.out.println(1+2);
-        System.out.println(1+2);
-        System.out.println(1+2);
+
+
+
         System.out.println(1+2);
     }
 
