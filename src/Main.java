65c261c820be05d05ae743566edce20b638e3c07
cosminciocan <ciocan.cosmin@gmail.com>
2019-02-06 20:27:42 +0200

    modificat ex1

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,27 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(1+2);
-        System.out.println(1+2);
+        int y = 4;
+        String nume = "Cosmin";
+
+        double adunare = x + 2.3f;
+        int x = 3;
+
+        System.out.println("prima adunare = " + (x + 2.3f));
+
+        adunare++;     // adunare = adunare + 1;
+        adunare += 3;  // adunare = adunare + 3;
+
+        System.out.println("A doua adunare = " + adunare);
+        int scadere = x - y;
+
+        System.out.println("Rezultatul scaderii dintre " + x + " si " + y + " este: " + scadere);
+
+        System.out.println(" 1+2 ");
+        System.out.println("Hello world!");
 
 
-
-        System.out.println(1+2);
+        System.out.println(1 + 2);
     }
 
 }
