3c51125d204a11e9af8095ad4022c491f72b185a
cosminciocan <ciocan.cosmin@gmail.com>
2019-02-06 19:56:45 +0200

    Am rezolvat ex 1 si 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(1+2);
+        System.out.println(1+2);
+        System.out.println(1+2);
+        System.out.println(1+2);
+        System.out.println(1+2);
+        System.out.println(1+2);
+        System.out.println(1+2);
+        System.out.println(1+2);
+        System.out.println(1+2);
     }
 
 }
